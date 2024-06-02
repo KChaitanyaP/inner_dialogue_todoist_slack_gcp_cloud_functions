c58816709ee8b1780c279a0f1c09155539a2fc7f
KChaitanyaP <>
2024-06-02 19:36:16 +0530

    bug fix

--- src/inner_dialogue_cloud_functions/dialogflow_agent_connect/main.py
+++ src/inner_dialogue_cloud_functions/dialogflow_agent_connect/main.py
@@ -1,51 +1,94 @@
+# Copyright 2018 Google LLC.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# [START functions_slack_setup]
+import os, json
+
+from flask import jsonify
 import functions_framework
-import json
-import os
-import requests
-from google.oauth2 import service_account
 from googleapiclient import discovery
+from google.oauth2 import service_account
+from slack.signature import SignatureVerifier
+from slack_sdk import WebClient
+from slack_sdk.webhook import WebhookClient
 from requests import Response
 import uuid
 
 
-@functions_framework.http
-def hello_http(request):
-    """HTTP Cloud Function.
-    Args:
-        request (flask.Request): The request object.
-        <https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data>
-    Returns:
-        The response text, or any set of values that can be turned into a
-        Response object using `make_response`
-        <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
-    """
-    request_json = request.get_json(silent=True)
-    request_args = request.args
+# [START functions_verify_webhook]
+def verify_signature(request):
+    request.get_data()  # Decodes received requests into request.data
 
-    if request_json and 'name' in request_json:
-        name = request_json['name']
-    elif request_args and 'name' in request_args:
-        name = request_args['name']
-    else:
-        name = 'World'
-    return 'Hello {}!'.format(name)
+    verifier = SignatureVerifier(os.environ["SLACK_SECRET"])
+
+    if not verifier.is_valid_request(request.data, request.headers):
+        raise ValueError("Invalid request/credentials.")
 
 
+# [END functions_verify_webhook]
 def _get_credentials():
     credentials = os.environ.get("CREDENTIALS")
     svc = json.loads(credentials.replace("\'", "\""))
     return service_account.Credentials.from_service_account_info(svc)
 
 
-def _upload_source_code_using_upload_url(upload_url: str, data):
-    # Prepare Header and data for PUT request
-    # https://cloud.google.com/functions/docs/reference/rest/v1/projects.locations.functions/generateUploadUrl
-    headers = {
-        "content-type": "application/zip"
+# [START functions_slack_format]
+def format_slack_message(query, response):
+    entity = None
+    if (
+            response
+            and response.get("itemListElement") is not None
+            and len(response["itemListElement"]) > 0
+    ):
+        entity = response["itemListElement"][0]["result"]
+
+    message = {
+        "response_type": "in_channel",
+        "text": f"Query: {query}",
+        "attachments": [],
     }
-    response: Response = requests.put(upload_url, headers=headers, data=data)
-    print(f"HTTP Status Code for uploading data: {response.status_code} \n")
-    print(f"Response body: {response.json} \n")
+
+    attachment = {}
+    if entity:
+        name = entity.get("name", "")
+        description = entity.get("description", "")
+        detailed_desc = entity.get("detailedDescription", {})
+        url = detailed_desc.get("url")
+        article = detailed_desc.get("articleBody")
+        image_url = entity.get("image", {}).get("contentUrl")
+
+        attachment["color"] = "#3367d6"
+        if name and description:
+            attachment["title"] = "{}: {}".format(entity["name"], entity["description"])
+        elif name:
+            attachment["title"] = name
+        if url:
+            attachment["title_link"] = url
+        if article:
+            attachment["text"] = article
+        if image_url:
+            attachment["image_url"] = image_url
+    else:
+        attachment["text"] = "No results match your query."
+    message["attachments"].append(attachment)
+
+    return message
+
+
+# [START functions_slack_request]
+def make_search_request(query):
+    res = query
+    return format_slack_message(query, res)
 
 
 def get_agents_list(service, parent):
@@ -75,15 +118,35 @@ def get_fulfillment(service, parent, match_intent_body):
     return fulfillment
 
 
-def _run(request):
-    text_input = request.form["text"] if 'text' in request.form.get else None
-    payload_input = json.loads(request.form.get('payload')) if 'payload' in request.form.get else None
+def create_session_id():
+    return uuid.uuid4()
+
+
+# [START functions_slack_search]
+@functions_framework.http
+def kg_search(request):
+    print("request form: ", request.form)
+    if request.method == 'POST':
+        data = request.get_json()
+        print("data: ", data)
+        # Responds to Slack's verification challenge
+        if 'challenge' in data:
+            verify_signature(request)
+            return jsonify({'challenge': data['challenge']})
+        else:
+            pass
+
+    # text_input = request.form["text"]
+    text_input = data['event']['blocks'][0]['elements'][0]['elements'][0]['text']
+    print("text_input: ", text_input)
+    # payload_input = json.loads(request.form.get('payload'))
+    # print("payload_input: ", payload_input)
     if text_input is not None:
         dialogflow_input = text_input
-    elif payload_input is not None:
-        dialogflow_input = payload_input['actions'][0]['value']
     else:
         dialogflow_input = ' '
+    # elif payload_input is not None:
+    #     dialogflow_input = payload_input['actions'][0]['value']
 
     gcp_project = "useful-proposal-424218-t8"  # os.environ.get("GCP_PROJECT")
     gcp_region = "global"  # os.environ.get("GCP_REGION")
@@ -92,8 +155,7 @@ def _run(request):
     loc_parent = f"projects/{gcp_project}/locations/{gcp_region}"
 
     service = discovery.build(
-        "dialogflow", "v3"
-        # , credentials=_get_credentials()
+        "dialogflow", "v3", credentials=_get_credentials()
     )
     # get_agents_list(service, loc_parent)
     # get_flows_list(service, f"projects/{gcp_project}/locations/{gcp_region}/agents/{agent_id}")
@@ -119,15 +181,27 @@ def _run(request):
             slack_args['blocks'] = response_message['payload']['blocks']
         else:
             pass
-    # print('Text Result:', dialogflowcx_response['queryResult']['responseMessages'][0]['text']['text'][0])
-    # print('Additional Payload:', dialogflowcx_response['queryResult']['responseMessages'][1]['payload'])
 
+    client = WebClient(token=os.environ['SLACK_OAUTH_TOKEN'])
+    # print("request: ", request)
+    # kg_search_response = make_search_request(request.form["text"])
+    # return jsonify(kg_search_response)
+    channel_id = 'D075T6SAUBD'
+    client.chat_postMessage(channel=channel_id, text=slack_args['text'], blocks=slack_args['blocks'])
+    # if payload['type'] == 'block_actions':
+    #    original_message = payload['actions'][0]['value']
+    #     client.chat_postMessage(channel=payload['channel']['id'], text=original_message)
+    #     client.chat_postMessage(channel=payload['channel']['id'], text=slack_args['text'], blocks=slack_args['blocks'])
 
-def create_session_id():
-    return uuid.uuid4()
+    # url = os.environ['SLACK_WEBHOOK_URL']
+    # webhook = WebhookClient(url)
+    # webhook.send(text=f"<@id_development> {original_message}")
+
+    # Send acknowledgment response
+    return '', 200
 
 
 if __name__ == '__main__':
-    _run()
+    app.run()
 
-# Adding this for testing the GitHub Action code 8 May 2024 evening when its heavily raining
+# [END functions_slack_search]
